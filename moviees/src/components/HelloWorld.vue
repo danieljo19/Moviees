d65d9cd655d40937892c409daf6eb94a5e6b4238
Fellyca <fellycaeffendi@mhs.mdp.ac.id>
2021-12-18 12:18:10 +0700

    Install

---
+++ moviees/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <button type="submit" class="btn btn-primary">
+      Start!
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
