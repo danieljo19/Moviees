ee1d632e295dbf0c2585111489cb6b39d292e7e5
danieljo19 <danieljohan37@gmail.com>
2021-12-19 20:22:48 +0700

    install tailwind

--- moviees/src/components/HelloWorld.vue
+++ moviees/src/components/HelloWorld.vue
@@ -1,17 +1,23 @@
 <template>
+<div>
+    <link href="/dist/output.css" rel="stylesheet">
   <div class="hello">
-    <button type="submit" class="btn btn-primary">
+    <button type="submit" class="border-solid decoration-pink-500 hover:decoration-blue-400">
       Start!
     </button>
+    <h1 class="text-gray-200 text-right">
+    Hello world!
+    </h1>
   </div>
+</div>
 </template>
 
 <script>
-export default {
+export default 
+{
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  props: 
+  {msg: String},
 }
 </script>
 
