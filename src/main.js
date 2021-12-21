819832be246614f7721cf70e5c149aed9430d7c7
JasenJonathan <kagaminomae@gmail.com>
2021-12-21 20:47:20 +0700

    carousel test

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import './input.css'
 
 createApp(App).mount('#app')
