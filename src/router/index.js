c24fdc653735adf78af86d089244bbe66ed6a6b5
JasenJonathan <kagaminomae@gmail.com>
2022-01-11 20:52:50 +0700

    router

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory} from "vue-router";
+
+const routes = [
+    {
+        Path: "/",
+        name: "dashboard",
+        component: () => import ('@/views/dashboard/Main.vue')
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: () => import ('@/views/about/Main.vue')
+    },
+    {
+        path: '/movies',
+        name: 'movies',
+        component: () => import ('@/views/moviees/Main.vue')
+    },
+    {
+        path: '/movies/:rank',
+        name: 'detailMovies',
+        component: () => import ('@/views/moviees/DetailMovie')
+    },
+    {
+        path: '/dashboard/:rank',
+        name: 'detailMovie1',
+        component: () => import ('@/views/dashboard/DetailMovie1')
+    }];
+    
+    const router = createRouter({
+        history: createWebHistory(),
+        routes
+    });
+    export default router;
