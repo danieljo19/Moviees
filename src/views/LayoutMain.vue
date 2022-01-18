c57a4b965421d4b722e69492917f4f37891cb95e
Fellyca <fellycaeffendi@mhs.mdp.ac.id>
2022-01-18 21:27:00 +0700

    Search Movie

--- src/views/LayoutMain.vue
+++ src/views/LayoutMain.vue
@@ -15,18 +15,7 @@
                         <a href="movies" class="m-py-4 m-px-2 m-font-semibold hover:m-text-yellow-500 m-transition m-duration-300">Movies</a>
                         <a href="about" class="m-py-4 m-px-2 m-font-semibold hover:m-text-yellow-500 m-transition m-duration-300">About</a>
                         <div class="m-relative m-mx-auto m-text-gray-600">
-                        <input class="m-bg-white m-h-10 m-px-5 m-pr-10 m-rounded-lg m-text-sm focus:m-outline-none"
-                            type="search" name="search" placeholder="Cari di Moviees">
-                        <button type="submit" class="m-absolute m-right-0 m-top-0 m-mt-3 m-mr-4"><a href="moviees/Search.vue">
-                            <svg class="m-text-gray-600 m-h-4 m-w-4 m-fill-current" xmlns="http://www.w3.org/2000/svg"
-                            xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
-                            viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
-                            width="512px" height="512px">
-                            <path
-                                d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-                            </svg>
-                            </a>
-                        </button>
+                    
                         </div>
                     </div>
                 </div>
@@ -39,6 +28,29 @@
 </template>
 <script>
 export default {
-    name : "LayoutMain"
+    name : "LayoutMain",
+data(){
+        return {
+            dataMovies: null
+        }
+    },
+    methods: 
+    {
+        getMovies()
+        {
+            fetch("https://imdb-api.com/en/API/Top250Movies/k_7tm6nttr")
+                .then(response => response.json())
+                .then(json => {
+                    this.dataMovies=json.items;
+                })
+                .catch(error =>
+                {
+                    console.log(error);
+                })
+        }
+    },
+    mounted(){
+        this.getMovies();
+    }
 }
 </script>
--- src/views/moviees/Main.vue
+++ src/views/moviees/Main.vue
@@ -4,14 +4,16 @@
             
         </layout-main>
         <div class="h1 m-mt-10 m-ml-5">ALL MOVIES</div>
-        <div v-for="movie in dataMovies" :key="movie" class="card; m-mx-2.5 m-my-5 m-bg-gray-100" style="width: 15%; height: 30%; float: left;">
+        <input class="m-bg-gray-200 m-h-10 m-px-5 m-ml-5 m-rounded-lg m-text-sm focus:m-outline-none" style="width:20%" v-model="search" type="search" name="search" placeholder="Cari di Moviees">
+        <br>
+        
+        <div v-for="movie in filteredMovies" :key="movie" class="card; m-mx-2.5 m-my-5 m-bg-gray-100" style="width: 15%; height: 30%; float: left;">
             <img :src="movie.image" style="width: 100%; height: 270px; margin: auto;">
             <div class="card-body">
                 <img src="star.svg" width="20" style="float: left; margin-right: 10px;">{{movie.imDbRating}}
                 <p style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">{{movie.title}}</p>
                 <router-link :to="{name:'detailMovies', params: {rank: movie.rank}}" class="btn btn-warning m-mt-2">Details</router-link>
             </div> 
-            
         </div>
     </div>
 </template>
@@ -24,14 +26,16 @@ export default {
     },
     data(){
         return {
-            dataMovies: null
+            dataMovies: null,
+            search:''
         }
     },
     methods: 
     {
-        getMovies()
-        {
-            fetch("https://imdb-api.com/en/API/Top250Movies/k_7tm6nttr")
+        
+    },
+    created(){
+        fetch("https://imdb-api.com/en/API/Top250Movies/k_7tm6nttr")
                 .then(response => response.json())
                 .then(json => {
                     this.dataMovies=json.items;
@@ -40,10 +44,13 @@ export default {
                 {
                     console.log(error);
                 })
-        }
     },
-    mounted(){
-        this.getMovies();
+    computed:{
+        filteredMovies: function(){
+            return this.dataMovies.filter((movie)=> {
+                return movie.title.match(this.search);
+            });
+        }
     }
 }
 </script>
