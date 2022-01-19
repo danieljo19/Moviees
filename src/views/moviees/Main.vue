f0c221615106ef2b7107f4b3c0799f80ee13f9cc
Fellyca <fellycaeffendi@mhs.mdp.ac.id>
2022-01-19 21:40:30 +0700

    Revert "Merge branch 'main' of https://github.com/danieljo19/Moviees"
    This reverts commit b08648868ec4b949a2872795139680f48c08aec5, reversing
    changes made to 400749de5c9a7c4b6c0c09fa414f08e99274f75c.

--- src/views/moviees/Main.vue
+++ src/views/moviees/Main.vue
@@ -3,24 +3,17 @@
         <layout-main>
             
         </layout-main>
-        <div class="h1 m-mt-5 m-ml-5 m-font-bold">All Movies</div>
+        <div class="h1 m-mt-10 m-ml-5">ALL MOVIES</div>
         <input class="m-bg-gray-200 m-h-10 m-px-5 m-ml-5 m-rounded-lg m-text-sm focus:m-outline-none" style="width:20%" v-model="search" type="search" name="search" placeholder="Cari di Moviees">
         <br>
-        <div v-for="movie in filteredMovies" :key="movie" class="card; m-mx-2.5 m-my-5 m-bg-gray-100 m-mb-20" style="width: 15%; height: 30%; float: left;">
+        
+        <div v-for="movie in filteredMovies" :key="movie" class="card; m-mx-2.5 m-my-5 m-bg-gray-100" style="width: 15%; height: 30%; float: left;">
             <img :src="movie.image" style="width: 100%; height: 270px; margin: auto;">
             <div class="card-body">
                 <img src="star.svg" width="20" style="float: left; margin-right: 10px;">{{movie.imDbRating}}
                 <p style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">{{movie.title}}</p>
                 <router-link :to="{name:'detailMovies', params: {rank: movie.rank}}" class="btn btn-warning m-mt-2">Details</router-link>
             </div> 
-        </div>
-        <div class="m-bottom-0 m-fixed m-w-full">
-        <footer class="m-bg-black m-text-center lg:m-text-left">
-            <div class="m-text-gray-400 m-text-center m-p-4">
-                © 2022 Copyright:
-                <a class="m-text-gray-400 hover:m-text-yellow-500 m-transition m-duration-300" href="about">Moviees</a>
-            </div>
-        </footer>
         </div>
     </div>
 </template>
@@ -42,7 +35,7 @@ export default {
         
     },
     created(){
-        fetch("https://imdb-api.com/en/API/Top250Movies/k_kzb59xch")
+        fetch("https://imdb-api.com/en/API/Top250Movies/k_7tm6nttr")
                 .then(response => response.json())
                 .then(json => {
                     this.dataMovies=json.items;
